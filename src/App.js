b7fe9e62eb1070ce3f112327e413675dac681005
guidovin <guidovin@gmail.com>
2020-08-17 05:56:50 -0300

    first draft, adds skeleton fileTree and fileEditor, using mocks and before styling

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import FileTreeView from './components/FileTreeView';
+import { fileTreeData, filesData } from "./mocks/dataMocks"
+import CustomTextEditor from './components/CustomTextEditor';
+import { withStyles } from '@material-ui/core';
+const classes = {
+  root: {
+    display: "flex"
+  },
+  fileTreeWrapper: {
+    width: "25vw"
+  },
+  textEditorWrapper: {
+    flexGrow: 1
+  }
+}
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      openId: ""
+    }
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  // the passing of a setter to a child node could be substituted by 
+  // global state management such as redux or even the new React context API
+  // in this instance and "project" scope, however, I believe it to be ill advised,
+  // too much unnecessary clutter and added complexity. 
+  setOpenId(newId){
+    console.log("set open ID to newId ", newId)
+    if(newId)
+      this.setState({ openId: newId });
+  }
+
+  getFileById(){
+    const file = filesData.filter( file => file.id == this.state.openId );
+    console.log(filesData, file[0], this.state.openId  )
+    if(file.length > 0)
+      return file[0];
+    else
+      return null;
+  }
+
+  render(){
+    const { classes } = this.props;
+    return (
+      <div className={classes.root}>
+        <div id="FileTreeViewWrapper" className={classes.fileTreeWrapper}>
+          <FileTreeView setOpenId={this.setOpenId.bind(this)} files={fileTreeData}/>
+        </div>
+        <div id="editorWrapper" className={classes.textEditorWrapper}>
+          {this.getFileById() && <CustomTextEditor file={this.getFileById()}/>}
+        </div>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default withStyles(classes)(App);
